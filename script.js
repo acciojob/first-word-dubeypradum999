0d6151aa1d68c805e41958651eb0bd42ce4f88ab
Pradumn Dubey <140749011+dubeypradum999@users.noreply.github.com>
2024-09-22 01:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim any leading or trailing spaces
+    str = str.trim();
+    
+    // Find the index of the first space
+    let firstSpaceIndex = str.indexOf(' ');
+    
+    // If there's no space, return the entire string
+    if (firstSpaceIndex === -1) {
+        return str;
+    }
+    
+    // Otherwise, return the substring from the start to the first space
+    return str.substring(0, firstSpaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Test cases
+console.log(firstWord('see and stop'));   // Output: 'see'
+console.log(firstWord(' Hello World!'));  // Output: 'Hello'
+console.log(firstWord('12345'));          // Output: '12345'
+console.log(firstWord(''));               // Output: ''
